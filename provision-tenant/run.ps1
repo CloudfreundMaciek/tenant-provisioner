5bb598df4f76a0d95fb3caf675edde25005fd883
CloudfreundMaciek <122986475+CloudfreundMaciek@users.noreply.github.com>
2024-10-09 14:58:16 +0200

    [Added] a function

---
+++ provision-tenant/run.ps1
@@ -0,0 +1,28 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+# Interact with query parameters or the body of the request.
+$SiteUrl = $Request.Body.SiteUrl
+$AppId = $Request.Body.AppId
+
+#if (-not $name) {
+#    $name = $Request.Body.Name
+#}
+
+$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+
+#Connect to SharePoint Online App Catalog site
+Connect-PnPOnline -Url $SiteUrl -ManagedIdentity -UserAssignedManagedIdentityClientId $AppId
+
+Add-PnPApp -Path ./aces_packages/demo-ace-1.sppkg -Publish -SkipFeatureDeployment
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $body
+})
